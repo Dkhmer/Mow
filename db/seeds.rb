11bc6ccd6dcca448695a1e86ab2f2a4d45ce9e2a
David Kuoch <kuoch0305@gmail.com>
2020-08-17 18:13:51 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Animal.create(name: 'Momo', specie: "sheep")
-Animal.create(name: 'Nono', specie: "cow")
-Animal.create(name: 'Toto', specie: "snake")
-Animal.create(name: 'Dodo', specie: "mouse")
-Animal.create(name: 'Zozo', specie: "dog")
-Animal.create(name: 'Soso', specie: "bear")
-Animal.create(name: 'Xoxo', specie: "pig")
-Animal.create(name: 'Coco', specie: "duck")
-Animal.create(name: 'Vovo', specie: "pikachu")
-puts " animal has been created"
+
+puts "Destroy Animals"
+Animal.destroy_all if Rails.env.development?
+
+
+puts "users"
+moloud = User.create!(email: 'Moloud@gmail.com', password: "sheeppig")
+jean = User.create!(email: 'Jean@gmail.com', password: "sheeppig")
+
+puts "users has been create"
+
+
+puts "create animals"
+Animal.create!(name: 'Momo', specie: "sheep", user: moloud)
+Animal.create!(name: 'Nono', specie: "cow", user: jean)
+# Animal.create(name: 'Toto', specie: "snake")
+# Animal.create(name: 'Dodo', specie: "mouse")
+# Animal.create(name: 'Zozo', specie: "dog")
+# Animal.create(name: 'Soso', specie: "bear")
+# Animal.create(name: 'Xoxo', specie: "pig")
+# Animal.create(name: 'Coco', specie: "duck")
+# Animal.create(name: 'Vovo', specie: "pikachu")
+puts " animal has been create"
