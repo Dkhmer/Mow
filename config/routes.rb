957dd43dddd232fd3c3a67a9979a8bdfbf868c67
David Kuoch <kuoch0305@gmail.com>
2020-08-17 16:26:55 +0200

    correction ressources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources: :animals do
-    resources: :bookings
+  resources :animals do
+    resources :bookings
   end
-  resources: :dashboard
+  resources :dashboard
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
