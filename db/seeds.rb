76a69a25220f210083901b25ab1f64b443fd16dc
Eric KUOCH <ek.kuoch@gmail.com>
2020-08-17 18:03:59 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Animal.destroy_all
+User.destroy_all
+puts "Creating animal........"
+
+
+narcisse = User.create!(email: "narcisse@gmail.com", password:"123456")
+leo = User.create!(email: "leo@gmail.com", password:"123456")
+
+Animal.create!(name: 'Momo', specie: "sheep", user: narcisse )
+Animal.create!(name: 'Nono', specie: "cow", user: leo)
+Animal.create(name: 'Toto', specie: "snake", user: narcisse)
+Animal.create(name: 'Dodo', specie: "mouse",user: narcisse )
+Animal.create(name: 'Zozo', specie: "dog",user: leo)
+Animal.create(name: 'Soso', specie: "bear",user: leo)
+Animal.create(name: 'Xoxo', specie: "pig",user: narcisse)
+Animal.create(name: 'Coco', specie: "duck",user: narcisse)
+Animal.create(name: 'Vovo', specie: "pikachu",user: narcisse)
+
