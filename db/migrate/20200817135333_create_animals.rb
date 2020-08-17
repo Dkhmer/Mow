8e289bb5c28209872c63d3f58430edaa1ad10984
David Kuoch <kuoch0305@gmail.com>
2020-08-17 15:56:22 +0200

    add animal model

---
+++ db/migrate/20200817135333_create_animals.rb
@@ -0,0 +1,15 @@
+class CreateAnimals < ActiveRecord::Migration[6.0]
+  def change
+    create_table :animals do |t|
+      t.string :name
+      t.string :specie
+      t.integer :age
+      t.integer :price
+      t.text :skill
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
