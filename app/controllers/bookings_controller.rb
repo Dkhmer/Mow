a7d2dce6f4581af87a012fb5ec5f9f38505a5aa4
Eric KUOCH <ek.kuoch@gmail.com>
2020-08-18 10:21:14 +0200

    createOrder en cours

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,7 @@ class BookingsController < ApplicationController
   def new
     @animal = Animal.find(params[:animal_id])
     @booking = Booking.new
+    @user = User.find(params[:user_id])
   end
 
   def show
@@ -11,15 +12,18 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @animal = Animal.find(params[:animal_id])
+    @user = User.find(params[:user_id])
     @booking.animal_id = @animal.id
+    @animal.user = @user
     @booking.save
+    raise
     redirect_to animal_path(@animal)
   end
 
   private
 
   def booking_params
-     params.require(:booking).permit(:user_id, :animal_id)
+     params.require(:booking).permit(:user_id, :animal_id, :start_date, :end_date, :id)
   end
 
 end
