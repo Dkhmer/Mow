a97fd85ddf0747e846d31830a80ad44ecf7a85fb
Narcisse Bosso <narcissebosso@yahoo.fr>
2020-08-20 18:27:08 +0200

    changes on show

--- app/javascript/components/calculate_price.js
+++ app/javascript/components/calculate_price.js
@@ -1,14 +1,27 @@
 const calculate_price = () => {
 
-  let start = Date.parse(document.getElementById("booking_start_date").value)
-  console.log(start)
-  let end = Date.parse(document.getElementById("booking_end_date").value)
-  console.log(end)
-  let duree = end - start
-  console.log(durée)
-  let total = durée * @animal.price
-  console.log(total)
-}
+
+  const start = document.getElementById("booking_start_date");
+  start.addEventListener("input",(event) => {
+    const dateStart = Date.parse(start.value);
+    console.log(dateStart);
+  });
+
+  const end = document.getElementById("booking_end_date");
+  end.addEventListener("input", (event) => {
+    const endStart = Date.parse(end.value);
+    console.log(endStart);
+  });
+
+  const total = endStart - dateStart
+
+  console.log(total);
+
+  // let duree = end - start
+  // console.log(durée)
+  // let total = durée * animal.price
+  // console.log(total)
+};
 
 
-export { calculate_price }
+export { calculate_price };
